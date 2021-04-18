c09243f20bd50646c67d107190cd492cff42e614
lotiko <l.despres@netc.fr>
2021-04-18 07:28:40 +0200

    iteration 4

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
-const express = require('express');
+const express = require("express");
 
-const hbs = require('hbs');
-const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const hbs = require("hbs");
+const { dirname } = require("path");
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/beers', (req, res, next) => {
+app.get("/beers", (req, res, next) => {
   punkAPI
     .getBeers()
-    .then(beersData => {
-      res.render('beers', { title: 'beers', data: beersData });
+    .then((beersData) => {
+      res.render("beers", { title: "beers", data: beersData });
     })
-    .catch(err => {
-      res.render('errors', { title: 'Oops', data: err });
+    .catch((err) => {
+      res.render("errors", { title: "Oops", data: err });
     });
 });
 
-app.get('/random-beers', (req, res, next) => {
-  res.render('random', { title: 'random' });
+app.get("/random-beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then((beersData) => {
+      res.render("random", {
+        title: "random",
+        beer: beersData[Math.floor(Math.random() * beersData.length)],
+      });
+    })
+    .catch((err) => {
+      res.render("errors", { title: "Oops", data: err });
+    });
 });
 
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'home' });
+app.get("/", (req, res) => {
+  res.render("index", { title: "home" });
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(3000, () => console.log("🏃‍ on port 3000"));
