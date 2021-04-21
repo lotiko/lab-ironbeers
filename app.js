d53f280a1807c0f1f8c8ecc7f14732df041f1260
lotiko <l.despres@netc.fr>
2021-04-21 12:07:21 +0200

    remove unused files

--- app.js
+++ app.js
@@ -9,56 +9,57 @@ const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
 hbs.registerPartials(__dirname + "/views/partials");
+app
+  .set("view engine", "hbs")
+  .set("views", path.join(__dirname, "views"))
 
-app.use(express.static(path.join(__dirname, "public")));
+  .use(express.static(path.join(__dirname, "public")))
 
-app.get("/beers", (req, res, next) => {
-  punkAPI
-    .getBeers()
-    .then((beersData) => {
-      console.log(beersData[10], "here");
-      res.render("beers", { title: "beers", data: beersData });
-    })
-    .catch((err) => {
-      res.render("errors", { title: "Oops", data: err });
-    });
-});
-app.get("/beers/:id", (req, res, next) => {
-  punkAPI
-    .getBeer(req.params.id)
-    .then((beerData) => {
-      console.log(beerData);
-      res.render("random", { title: "beer details", beer: { ...beerData[0], random: true } });
-    })
-    .catch((err) => {
-      res.render("errors", { title: "Oops", data: err });
-    });
-});
-
-app.get("/random-beers", (req, res, next) => {
-  punkAPI
-    .getBeers()
-    .then((beersData) => {
-      console.log(beersData[0]);
-      res.render("random", {
-        title: "random",
-        beer: { ...beersData[Math.floor(Math.random() * beersData.length)], random: true },
+  .get("/beers", (req, res, next) => {
+    punkAPI
+      .getBeers()
+      .then((beersData) => {
+        console.log(beersData[10], "here");
+        res.render("beers", { title: "beers", data: beersData });
+      })
+      .catch((err) => {
+        res.render("errors", { title: "Oops", data: err });
       });
-    })
-    .catch((err) => {
-      res.render("errors", { title: "Oops", data: err });
-    });
-});
+  })
+  .get("/beers/:id", (req, res, next) => {
+    punkAPI
+      .getBeer(req.params.id)
+      .then((beerData) => {
+        console.log(beerData);
+        res.render("random", { title: "beer details", beer: { ...beerData[0], random: true } });
+      })
+      .catch((err) => {
+        res.render("errors", { title: "Oops", data: err });
+      });
+  })
 
-// ...
+  .get("/random-beers", (req, res, next) => {
+    punkAPI
+      .getBeers()
+      .then((beersData) => {
+        console.log(beersData[0]);
+        res.render("random", {
+          title: "random",
+          beer: { ...beersData[Math.floor(Math.random() * beersData.length)], random: true },
+        });
+      })
+      .catch((err) => {
+        res.render("errors", { title: "Oops", data: err });
+      });
+  })
 
-// Add the route handlers here:
+  // ...
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "home" });
-});
+  // Add the route handlers here:
 
-app.listen(3000, () => console.log("🏃‍ on port 3000"));
+  .get("/", (req, res) => {
+    res.render("index", { title: "home" });
+  })
+
+  .listen(3000, () => console.log("🏃‍ on port 3000"));
