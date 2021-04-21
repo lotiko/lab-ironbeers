d96b2bda6622b31ad8cc62cf951d8ed731c6342d
lotiko <l.despres@netc.fr>
2021-04-21 12:14:46 +0200

    refactor app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const { throws } = require("assert");
 const express = require("express");
-
 const hbs = require("hbs");
-const { dirname } = require("path");
 const path = require("path");
 const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 
@@ -13,53 +10,32 @@ hbs.registerPartials(__dirname + "/views/partials");
 app
   .set("view engine", "hbs")
   .set("views", path.join(__dirname, "views"))
-
   .use(express.static(path.join(__dirname, "public")))
-
   .get("/beers", (req, res, next) => {
     punkAPI
       .getBeers()
-      .then((beersData) => {
-        console.log(beersData[10], "here");
-        res.render("beers", { title: "beers", data: beersData });
-      })
-      .catch((err) => {
-        res.render("errors", { title: "Oops", data: err });
-      });
+      .then((beersData) => res.render("beers", { title: "beers", data: beersData }))
+      .catch((err) => res.render("errors", { title: "Oops", data: err }));
   })
   .get("/beers/:id", (req, res, next) => {
     punkAPI
       .getBeer(req.params.id)
-      .then((beerData) => {
-        console.log(beerData);
-        res.render("random", { title: "beer details", beer: { ...beerData[0], random: true } });
-      })
-      .catch((err) => {
-        res.render("errors", { title: "Oops", data: err });
-      });
+      .then((beerData) =>
+        res.render("random", { title: "beer details", beer: { ...beerData[0], random: true } })
+      )
+      .catch((err) => res.render("errors", { title: "Oops", data: err }));
   })
-
   .get("/random-beers", (req, res, next) => {
     punkAPI
       .getBeers()
-      .then((beersData) => {
-        console.log(beersData[0]);
+      .then((beersData) =>
         res.render("random", {
           title: "random",
           beer: { ...beersData[Math.floor(Math.random() * beersData.length)], random: true },
-        });
-      })
-      .catch((err) => {
-        res.render("errors", { title: "Oops", data: err });
-      });
+        })
+      )
+      .catch((err) => res.render("errors", { title: "Oops", data: err }));
   })
 
-  // ...
-
-  // Add the route handlers here:
-
-  .get("/", (req, res) => {
-    res.render("index", { title: "home" });
-  })
-
+  .get("/", (req, res) => res.render("index", { title: "home" }))
   .listen(3000, () => console.log("🏃‍ on port 3000"));
